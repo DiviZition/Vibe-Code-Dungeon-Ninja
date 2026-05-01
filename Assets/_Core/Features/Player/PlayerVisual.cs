08ae0ec3ceaeaf5e62a4a16945be72055ea7f0d6
DiviZition <vitya.popo0201@gmail.com>
2026-05-01 19:38:22 +0300

    Player visual jittering bug is fixed;

--- Assets/_Core/Features/Player/PlayerVisual.cs
+++ Assets/_Core/Features/Player/PlayerVisual.cs
@@ -6,19 +6,17 @@ namespace Player
     public class PlayerVisual : MonoBehaviour
     {
         [Header("References")]
-        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Transform _visualTransform;
 
         [Header("Settings")]
         [SerializeField] private float _rotationDuration = 0.1f;
-
-
 
         public void SetDirection(Vector2 direction)
         {
             if (direction != Vector2.zero)
             {
                 float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
-                transform.DORotate(new Vector3(0, 0, angle), _rotationDuration);
+                _visualTransform.DORotate(new Vector3(0, 0, angle), _rotationDuration);
             }
         }
     }
